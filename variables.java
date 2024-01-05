6dc29d274a23fd731232dbd65fceea7e31059288
Alen <alenbeljimini@gmail.com>
2024-01-05 23:02:10 +0530

    Integers

---
+++ variables.java
@@ -0,0 +1,32 @@
+public class variables {
+    public static void main(String args[]){
+        //Integer
+        byte a=1;
+        short b=2;
+        int c=10;
+        long d=100l;
+
+        //Float
+        float m=10.9f;
+        double n=12.09890;
+
+        //Character
+        char x='x';
+
+        //Boolean
+        boolean y=true;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println("_________________");
+        System.out.println(m);
+        System.out.println(n);
+        System.out.println("_________________");
+        System.out.println(x);
+        System.out.println("_________________");
+        System.out.println(y);
+    }
+    
+}
