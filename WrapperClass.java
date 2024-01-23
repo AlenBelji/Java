c44b5422dc15d8d99a977f17ac9d55ae51abce13
Alen <alenbeljimini@gmail.com>
2024-01-23 11:15:42 +0530

    Wrapper class and parse int

---
+++ WrapperClass.java
@@ -0,0 +1,13 @@
+public class WrapperClass {
+    public static void main(String[] args) {
+        Integer i=10;
+        String x="Clark";
+        Double j=12.434;
+        String y="123";
+        System.out.println(i);
+        System.out.println(x);
+        System.out.println(j);
+        int k=Integer.parseInt(y);
+        System.out.println(k);
+    }
+}
