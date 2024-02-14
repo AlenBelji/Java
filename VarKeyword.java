a0046f5b7113504c10b384de28899edb2e3350d7
Alen <alenbeljimini@gmail.com>
2024-02-14 23:58:10 +0530

    Var keyword in java

---
+++ VarKeyword.java
@@ -0,0 +1,13 @@
+public class VarKeyword {
+    public static void main(String args[]){
+        var a="This is a string";  //var can be used to declare variables without datatype
+        var b=123;
+        var c=21.3233;
+        var d=true;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        if(d)
+            System.out.println(d);
+    }
+}
